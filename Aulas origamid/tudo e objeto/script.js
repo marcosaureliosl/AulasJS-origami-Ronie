0cdd35894801620eabee647a016176217edb5a9d
Marcos Aurelio <marcosaureliosl@gmail.com>
2023-12-15 02:09:26 -0300

    aulas JS

---
+++ Aulas origamid/tudo e objeto/script.js
@@ -0,0 +1,55 @@
+
+
+var nome = 'André';
+
+nome.length; // 5
+nome.charAt(1); // 'n'
+nome.replace('ré', 'rei'); // 'Andrei'
+nome; // 'André'
+
+console.log(nome); 
+console.log(nome.length); 
+console.log(nome.charAt(1)); 
+console.log(nome[2] , nome[4]);
+console.log(nome[2] + nome[3]);
+console.log(nome.replace('ré', 'rea'))
+
+
+// var altura = 1.8;
+
+// altura.toString(); // '1.8'
+// altura.toFixed(); // '2'
+
+
+// function areaQuadrado(lado) {
+//     return lado * lado;
+//   }
+  
+//   areaQuadrado.toString();
+//   //"function areaQuadrado(lado) {
+//   //  return lado * lado;
+//   //}"
+  
+//   areaQuadrado.length; // 1
+  
+
+//   <a class="btn">Clique</a>
+
+//   var btn = document.querySelector('.btn');
+
+//   btn.classList.add('azul') // adiciona a classe azul
+//   btn.innerText; // 'Clique'
+//   btn.addEventListener('click', function() {
+//     console.log('Clicou')
+//   })
+
+  
+
+  
+//   // nomeie 3 propriedades ou métodos de strings
+
+// // nomeie 5 propriedades ou métodos de elementos do DOM
+
+// // busque na web um objeto (método) capaz de interagir com o clipboard, 
+// // clipboard é a parte do seu computador que lida com o CTRL + C e CTRL + V
+
